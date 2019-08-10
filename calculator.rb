cb266f11c1f14aeec023f7b55872e003c8eba1c0
daisy Kiyemba <deedaisy29@gmail.com>
2019-08-10 20:46:37 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,7 +7,7 @@ second_number = 8
 
 sum = first_number+ second_number
 
-difference = first_number-learn testsecond_number
+difference = first_number-second_number
   
 product = first_number* second_number
   

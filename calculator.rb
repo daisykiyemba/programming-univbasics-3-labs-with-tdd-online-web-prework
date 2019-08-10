1218feaad63ff2c4ad2eccd95b43affeff959807
daisy Kiyemba <deedaisy29@gmail.com>
2019-08-10 20:41:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,42 +2,16 @@
 
 first_number = 7
 
+second_number = 8
 
-def
-second_number = get_variable_from_file('./calculator.rb', "second_number")
 
-expect(second_number).to be_an(Interger).or be_a(Float)
-expect(second_number).not_to equal(0)
-end
-
-def
   sum = ("first_number" + "second_number")
-  
-  expect(sum).to eq(first_number+second_number)
-  
-end
 
-
-def
   difference = ("first_number" + "second_number")
   
-  expect(difference).to eq(first_number-second_number)
-  
-end
-
-
-def
   product = ("first_number" * "second_number")
   
-  expect(product).to eq(first_number*second_number)
-  
-end
-
-
-def
 quotient = ("first_number" / "second_number")
 
-expect(quotient).to eq(first_number/second_number)
 
-end
 

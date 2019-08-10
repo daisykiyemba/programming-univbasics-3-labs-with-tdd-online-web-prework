8d4463ce7af4014bcaa577073e4a8207e920a53c
daisy Kiyemba <deedaisy29@gmail.com>
2019-08-10 20:44:34 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,13 +5,13 @@ first_number = 7
 second_number = 8
 
 
-  sum = ("first_number" + "second_number")
+sum = first_number+ second_number
 
-  difference = ("first_number" + "second_number")
+difference = first_number+second_number
   
-  product = ("first_number" * "second_number")
+product = first_number* second_number
   
-quotient = ("first_number" / "second_number")
+quotient = first_number /second_number
 
 
 
